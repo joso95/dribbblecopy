a2b76a24f8aa3ad5258124fb3d9bce482d9226ef
Jorgen Solum <j-solum@live.com>
2020-10-22 15:34:45 +0200

    json and cardcomp changes
    added picture urls to json, started to impliment pcitures in cardcomp

--- dribbblecopy/src/components/mainContent.js
+++ dribbblecopy/src/components/mainContent.js
@@ -2,11 +2,12 @@ import React from 'react'
 import './MainContent.css'
 import './Grid.css'
 import CardComponent from './cardComponent'
+import InfoFromDribbble from './infoForDribbble.json'
 
 const MainContent=()=>{
     return(
         <div className='wrapper'>
-            <CardComponent imgSrc='#' alt='#' title='yes' text='no'/>
+            <CardComponent imgSrc={InfoFromDribbble.pictures.thumbnails} alt='#' title='yes' text='no'/>
             <CardComponent imgSrc='#' alt='#' title='yes' text='no'/>
         </div>
     )
