bace8ceaa20abc96a2b05b27b1ab9801af980f08
bak009 <j-solum@live.com>
2020-10-25 21:32:14 +0100

    changes to files
    js -> jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import Header from './components/js/header'
-import Intro from './components/js/introduction'
-import SubNav from './components/js/subNav'
-import MainContent from './components/js/mainContent'
+import Header from './components/js/header.jsx'
+import Intro from './components/js/introduction.jsx'
+import SubNav from './components/js/subNav.jsx'
+import MainContent from './components/js/mainContent.jsx'
 
 function App() {
   return (
